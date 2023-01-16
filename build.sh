8422c0358746e6f0bd3a0bb024c10254cadb7727
Mayank Agrawal <mayankagrawal@spliceglobal.com>
2023-01-16 23:57:26 +0530

    SuperUser Deploy

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py migrate
-python manage.py createsu
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=Mayank@16Splice \
+DJANGO_SUPERUSER_EMAIL="mayankagrawal6162@gmail.com" \
+python manage.py createsuperuser --noinput
